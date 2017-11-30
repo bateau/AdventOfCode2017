3a11dee83829c112449b55db9a72ebb251436474
Michael Loughry 👨‍💻 <miclo@microsoft.com>
2017-11-30 14:07:57 -0800

    Boilerplate start

---
+++ src/day25/index.ts
@@ -0,0 +1,7 @@
+import * as fs from 'mz/fs';
+
+async function findSolution(inputFilePath: string = './input.txt') {
+    const input = await fs.readFile(require.resolve(inputFilePath), 'utf8');
+}
+
+findSolution();
