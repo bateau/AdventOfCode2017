eb3873420bb28fdd215008050510eddfc5a139e7
Michael Loughry <miclo@microsoft.com>
2017-11-30 21:58:06 -0800

    Day 1

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -1,7 +1,28 @@
 import * as fs from 'mz/fs';
 
 async function findSolution(inputFilePath: string = './input.txt') {
-    const input = await fs.readFile(require.resolve(inputFilePath), 'utf8');
+    const input: string = await fs.readFile(require.resolve(inputFilePath), 'utf8');
+
+    const numStream = parseInput(input);
+    const streamSize = numStream.length;
+
+    function calculateCaptcha(offset: number) {
+        let runningTotal = 0;
+        for (let i = 0; i < streamSize; i++) {
+            if (numStream[i] === numStream[(i + offset) % streamSize]) {
+                runningTotal += numStream[i];
+            }
+        }
+
+        return runningTotal;
+    }
+
+    console.log(`Part 1: ${calculateCaptcha(1)}`);
+    console.log(`Part 2: ${calculateCaptcha(streamSize / 2)}`);
+}
+
+function parseInput(input: string): number[] {
+    return input.split('').map(chr => parseInt(chr));
 }
 
 findSolution();
