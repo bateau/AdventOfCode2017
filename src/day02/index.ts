8d805667c99f6a868a88016b5bcf283202e2f0f7
Michael Loughry <miclo@microsoft.com>
2017-12-02 10:44:54 -0800

    Day 2 cleanup

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -3,35 +3,42 @@ import * as fs from 'mz/fs';
 async function findSolution(inputFilePath: string = './input.txt') {
     const input = await fs.readFile(require.resolve(inputFilePath), 'utf8');
 
+    const grid: number[][] = parseInput(input);
+
+    console.log(`Part 1: ${solvePart1(grid)}`);
+    console.log(`Part 2: ${solvePart2(grid)}`);
+}
+
+function parseInput(input: string): number[][] {
     const grid: number[][] = input
         .split('\n')
-        .map(line =>
-            line
-                .split('\t')
-                .map(cell =>
-                    parseInt(cell)));
+        .map(line => line
+            .split('\t')
+            .map(cell => parseInt(cell)));
+    return grid;
+}
 
+function solvePart1(grid: number[][]) {
     let checksum = 0;
     grid.forEach(line => {
-        let sortedLine = [...line].sort((a, b) => a - b);
-        checksum += sortedLine[sortedLine.length - 1] - sortedLine[0]
+        checksum += Math.max(...line) - Math.min(...line);
     });
+    return checksum;
+}
 
-    console.log(`Part 1: ${checksum}`);
-
-    checksum = 0;
+function solvePart2(grid: number[][]) {
+    let checksum = 0;
     grid.forEach(line => {
         line.forEach(divisor => {
-            line.forEach(dividee => {
-                if (divisor !== dividee
-                    && Number.isInteger(dividee / divisor)) {
-                        checksum += dividee / divisor
-                    }
+            line.forEach(dividend => {
+                if (divisor !== dividend
+                    && Number.isInteger(dividend / divisor)) {
+                    checksum += dividend / divisor;
+                }
             });
         });
     });
-
-    console.log(`Part 2: ${checksum}`);
+    return checksum;
 }
 
 findSolution();
