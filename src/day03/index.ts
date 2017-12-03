8dc79b10af016c06c19209ff4be985b99be8bf2a
Michael Loughry <miclo@microsoft.com>
2017-12-03 09:04:57 -0800

    Day 3 WIP

--- src/day03/index.ts
+++ src/day03/index.ts
@@ -1,7 +1,54 @@
 import * as fs from 'mz/fs';
 
 async function findSolution(inputFilePath: string = './input.txt') {
-    const input = await fs.readFile(require.resolve(inputFilePath), 'utf8');
+    const input = parseInt(await fs.readFile(require.resolve(inputFilePath), 'utf8'));
+
+    console.log(`Part1: ${solvePart1(input)}`);
+}
+
+function solvePart1(input: number) : number {
+    // Find which ring the input number is on
+    // Each ring has 8*(ring number) elements on it
+    var { remainder, ring } = findCellOffsetDimensions(input);
+
+    const quarterRingSize = ring * 2 || 1;
+    let offset = (remainder - (ring - 1)) % quarterRingSize;
+    let answer = Math.min(offset, quarterRingSize - offset) + ring;
+    return answer;
+}
+
+interface MemoryCell {
+    value: number;
+    neighbors: number[];
+}
+
+function findCellOffsetDimensions(input: number) {
+    let ring = 0;
+    let step = 0;
+    let countdown = input - 1;
+    for (; countdown > 0; countdown -= step) {
+        step += 8;
+        ring++;
+    }
+    let remainder = countdown + step - 1;
+    return { remainder, ring };
+}
+
+function solvePart2(input: number): number {
+    let memory: MemoryCell[] = [];
+
+    memory[1] = {
+        value: 1,
+        neighbors: findNeighbors(1)
+    };
+
+    // TODO: Finish
+    return 0;
+
+    function findNeighbors(cell: number): number[] {
+        // TODO: Finish
+        return [];
+    }
 }
 
 findSolution();
