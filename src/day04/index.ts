75e846b7c27cb265edaa6f151e00afe81c2e27a9
Michael Loughry <miclo@microsoft.com>
2017-12-03 21:20:35 -0800

    Day 4

--- src/day04/index.ts
+++ src/day04/index.ts
@@ -2,6 +2,39 @@ import * as fs from 'mz/fs';
 
 async function findSolution(inputFilePath: string = './input.txt') {
     const input = await fs.readFile(require.resolve(inputFilePath), 'utf8');
+
+    const phrases = input.split('\n');
+
+    console.log(`Part 1: ${solvePart1(phrases)}`);
+    console.log(`Part 2: ${solvePart2(phrases)}`);
 }
 
 findSolution();
+
+
+function solvePart1(phrases: string[]): number {
+    return findNumberOfDuplicateWords(phrases);
+}
+
+function solvePart2(phrases: string[]): number {
+    return findNumberOfDuplicateWords(phrases
+        .map(phrase =>
+            phrase
+                .split(' ')
+                .map(word =>
+                    word
+                        .split('')
+                        .sort()
+                        .join(''))
+                .sort()
+                .join(' ')));
+}
+
+function findNumberOfDuplicateWords(phrases: string[]): number {
+    return phrases.reduce((acc: number, phrase: string) => {
+        if ((/(\b\w+\b).*\b\1\b/).test(phrase)) {
+            return acc;
+        }
+        return acc + 1;
+    }, 0);
+}
