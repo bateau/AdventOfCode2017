1c1691c0eea5538dac5028efda713927d5daa2ed
Michael Loughry <miclo@microsoft.com>
2017-12-10 21:26:46 -0800

    Much cleaner day 11

--- src/day11/index.ts
+++ src/day11/index.ts
@@ -1,49 +1,44 @@
 import * as fs from 'mz/fs';
 
+interface HexCoordinate {
+    x: number;
+    y: number;
+    z: number;
+}
+
 async function findSolution(inputFilePath: string = './input.txt') {
     const input = await fs.readFile(require.resolve(inputFilePath), 'utf8');
 
-    const opposites: { [key: string]: string} = {
-        n: 's',
-        s: 'n',
-        ne: 'sw',
-        se: 'nw',
-        nw: 'se',
-        sw: 'ne',
-    };
-
-    const consolidation: { [key: string]: string} = {
-        ne: 'nw',
-        se: 'sw',
-        nw: 'ne',
-        sw: 'se',
-    };
-
     let directions: string[] = input.split(',');
 
-    for (let i = 0; i < directions.length; ) {
-        let oppositeDirectionIndex = directions.indexOf(opposites[directions[i]]);
-        if (oppositeDirectionIndex !== -1) {
-            directions.splice(oppositeDirectionIndex, 1);
-            directions.splice(i, 1);
-        } else {
-            i++;
-        }
-    }
+    // Thanks to https://www.redblobgames.com/grids/hexagons/#coordinates-cube for the guidance on this algorithm
+    const directionToCoordinate: { [direction: string]: (coord: HexCoordinate) => HexCoordinate } = {
+        n: (coord: HexCoordinate) => ({x: coord.x, y: coord.y + 1, z: coord.z - 1}),
+        s: (coord: HexCoordinate) => ({x: coord.x, y: coord.y - 1, z: coord.z + 1}),
+        ne: (coord: HexCoordinate) => ({x: coord.x + 1, y: coord.y, z: coord.z - 1}),
+        sw: (coord: HexCoordinate) => ({x: coord.x - 1, y: coord.y, z: coord.z + 1}),
+        nw: (coord: HexCoordinate) => ({x: coord.x - 1, y: coord.y + 1, z: coord.z}),
+        se: (coord: HexCoordinate) => ({x: coord.x + 1, y: coord.y - 1, z: coord.z}),
+    };
 
-    for (let i = 0; i < directions.length; ) {
-        let consolidateIndex = directions.indexOf(consolidation[directions[i]]);
-        if (consolidateIndex !== -1) {
-            directions.splice(consolidateIndex, 1);
-            directions.splice(i, 1, directions[i].substr(0, 1));
-        } else {
-            i++;
-        }
-    }
+    let maxDistance = 0;
+    let currentPosition = { x: 0, y: 0, z: 0};
 
-    console.log(JSON.stringify(directions));
-    console.log(directions.length);
+    directions.forEach(dir => {
+        currentPosition = directionToCoordinate[dir](currentPosition);
+        maxDistance = Math.max(maxDistance, distanceFromOrigin(currentPosition));
+    });
+
+    console.log(`Part 1: ${distanceFromOrigin(currentPosition)}`);
+    console.log(`Part 2: ${maxDistance}`);
 }
-// 375 ne, 330 se, 72 s
+
+function distanceFromOrigin(coord: HexCoordinate): number {
+    return Math.max(
+        Math.abs(coord.x),
+        Math.abs(coord.y),
+        Math.abs(coord.z),
+    )
+}
 
 findSolution();
