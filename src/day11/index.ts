47bf6d15b8ee15da2abea4fb38a5824c70200b4f
Michael Loughry <miclo@microsoft.com>
2017-12-10 21:18:33 -0800

    Hacked up day 11, part 1

--- src/day11/index.ts
+++ src/day11/index.ts
@@ -2,6 +2,48 @@ import * as fs from 'mz/fs';
 
 async function findSolution(inputFilePath: string = './input.txt') {
     const input = await fs.readFile(require.resolve(inputFilePath), 'utf8');
+
+    const opposites: { [key: string]: string} = {
+        n: 's',
+        s: 'n',
+        ne: 'sw',
+        se: 'nw',
+        nw: 'se',
+        sw: 'ne',
+    };
+
+    const consolidation: { [key: string]: string} = {
+        ne: 'nw',
+        se: 'sw',
+        nw: 'ne',
+        sw: 'se',
+    };
+
+    let directions: string[] = input.split(',');
+
+    for (let i = 0; i < directions.length; ) {
+        let oppositeDirectionIndex = directions.indexOf(opposites[directions[i]]);
+        if (oppositeDirectionIndex !== -1) {
+            directions.splice(oppositeDirectionIndex, 1);
+            directions.splice(i, 1);
+        } else {
+            i++;
+        }
+    }
+
+    for (let i = 0; i < directions.length; ) {
+        let consolidateIndex = directions.indexOf(consolidation[directions[i]]);
+        if (consolidateIndex !== -1) {
+            directions.splice(consolidateIndex, 1);
+            directions.splice(i, 1, directions[i].substr(0, 1));
+        } else {
+            i++;
+        }
+    }
+
+    console.log(JSON.stringify(directions));
+    console.log(directions.length);
 }
+// 375 ne, 330 se, 72 s
 
 findSolution();
