9074c5bbc25c9966f9cbe9a1f1f4d49d9411380e
Michael Loughry <miclo@microsoft.com>
2017-12-12 07:53:53 -0800

    Day 12

--- src/day12/index.ts
+++ src/day12/index.ts
@@ -1,7 +1,55 @@
 import * as fs from 'mz/fs';
 
+const inputLineRegex = /(\d+) <-> (\d+(, \d+)*)/;
+
 async function findSolution(inputFilePath: string = './input.txt') {
     const input = await fs.readFile(require.resolve(inputFilePath), 'utf8');
+
+    let graph: number[][] = buildGraph(input);
+    let connectedGraphs = findConnectedGraphs(graph);
+
+    console.log(`Part 1: ${connectedGraphs[0].length}`);
+    console.log(`Part 1: ${connectedGraphs.length}`);
 }
 
 findSolution();
+
+function findConnectedGraphs(graph: number[][]): number[][] {
+    let connectedGraphs: number[][] = [];
+    let nodeTracker: boolean[] = []
+    let queue: number[] = [0];
+    let groupNumber = 0;
+
+    for (let i = 0; i < graph.length; i++) {
+        if (nodeTracker[i]) {
+            continue;
+        }
+
+        connectedGraphs[groupNumber] = [];
+        queue = [i];
+        while (queue.length) {
+            let nodeId = queue.shift()!;
+            if (!nodeTracker[nodeId]) {
+                connectedGraphs[groupNumber].push(nodeId);
+                nodeTracker[nodeId] = true;
+                queue = queue.concat(graph[nodeId]);
+            }
+        }
+        groupNumber++;
+    }
+
+    return connectedGraphs;
+}
+
+function buildGraph(input: string) {
+    let nodes: number[][] = [];
+    input
+        .split('\n')
+        .forEach(line => {
+            let [match, nodeId, connectedNodes] = inputLineRegex.exec(line)!;
+            nodes[parseInt(nodeId)] = connectedNodes
+                .split(', ')
+                .map(id => parseInt(id));
+        });
+    return nodes;
+}
