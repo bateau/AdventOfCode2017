be52ab9d042b35deafe0ccb6fa984d700ce854d7
Michael Loughry <miclo@microsoft.com>
2017-12-13 21:30:00 -0800

    Day 14, part 1

--- src/day10/index.ts
+++ src/day10/index.ts
@@ -1,16 +1,22 @@
 import * as fs from 'mz/fs';
+const loopSize = 256;
 
 async function findSolution(inputFilePath: string = './input.txt') {
     const input = await fs.readFile(require.resolve(inputFilePath), 'utf8');
 
     let hashLengthsPart1 = input.split(',').map(length => parseInt(length));
-    let hashLengthsPart2 = input.split('').map(chr => chr.charCodeAt(0)).concat([17, 31, 73, 47, 23]);
-    const loopSize = 256;
     let list = new Array(loopSize).fill(0, 0, loopSize).map((val, i) => i);
 
     let hashedListPart1 = hash(list, hashLengthsPart1);
     console.log(`Part 1: ${hashedListPart1[0] * hashedListPart1[1]}`);
 
+    let hashCode = knotHash(input);
+    console.log(`Part 2: ${hashCode}`);
+}
+
+export function knotHash(input: string) {
+    let list = new Array(loopSize).fill(0, 0, loopSize).map((val, i) => i);
+    let hashLengthsPart2 = input.split('').map(chr => chr.charCodeAt(0)).concat([17, 31, 73, 47, 23]);
     let hashedListPart2 = hash(list, hashLengthsPart2, 64);
     let denseHash: number[] = [];
     hashedListPart2.forEach((val: number, index: number) => {
@@ -22,7 +28,7 @@ async function findSolution(inputFilePath: string = './input.txt') {
             let digits = val.toString(16);
             return digits.length === 1 ? `0${digits}` : digits;
         }).join('');
-    console.log(`Part 2: ${hashCode}`);
+    return hashCode;
 }
 
 function hash(list: number[], hashLengths: number[], iterations: number = 1): number[] {
--- src/day14/index.ts
+++ src/day14/index.ts
@@ -1,7 +1,50 @@
 import * as fs from 'mz/fs';
+import { knotHash } from '../day10/index';
 
 async function findSolution(inputFilePath: string = './input.txt') {
     const input = await fs.readFile(require.resolve(inputFilePath), 'utf8');
+
+    let usedSquares = 0;
+    for (let i = 0; i < 128; i++) {
+        let seed = `${input}-${i}`;
+        let hashString = knotHash(seed);
+        let bits = 0;
+        hashString.split('').forEach(digit => {
+            bits += countBitsInHexDigit(digit);
+        });
+        console.log(`${seed} -> ${hashString} -> ${bits}`);
+        usedSquares += bits
+    }
+
+    console.log(usedSquares);
+}
+
+function countBitsInHexDigit(hexDigit: string) {
+    switch (hexDigit) {
+        case '0':
+            return 0;
+        case '1':
+        case '2':
+        case '4':
+        case '8':
+            return 1;
+        case '3':
+        case '5':
+        case '9':
+        case '6':
+        case 'a':
+        case 'c':
+            return 2;
+        case '7':
+        case 'b':
+        case 'd':
+        case 'e':
+            return 3;
+        case 'f':
+            return 4;
+    }
+
+    throw new Error('Invalid hex digit');
 }
 
 findSolution();
