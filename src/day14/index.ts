13e77e9dfbf8c15cf8167d77788e7da751b7052c
Michael Loughry <miclo@microsoft.com>
2017-12-13 22:16:58 -0800

    Finish day 14.
    Should clean up import

--- src/day14/index.ts
+++ src/day14/index.ts
@@ -4,47 +4,81 @@ import { knotHash } from '../day10/index';
 async function findSolution(inputFilePath: string = './input.txt') {
     const input = await fs.readFile(require.resolve(inputFilePath), 'utf8');
 
-    let usedSquares = 0;
+    let grid: number[][] = [];
+
     for (let i = 0; i < 128; i++) {
         let seed = `${input}-${i}`;
         let hashString = knotHash(seed);
-        let bits = 0;
-        hashString.split('').forEach(digit => {
-            bits += countBitsInHexDigit(digit);
-        });
-        console.log(`${seed} -> ${hashString} -> ${bits}`);
-        usedSquares += bits
+        grid[i] = setBitsInRow(hashString);
     }
 
-    console.log(usedSquares);
+    console.log(`Part 1: ${countUsedCells(grid)}`);
+    console.log(`Part 2: ${countGroupsInGrid(grid)}`);
 }
 
-function countBitsInHexDigit(hexDigit: string) {
-    switch (hexDigit) {
-        case '0':
-            return 0;
-        case '1':
-        case '2':
-        case '4':
-        case '8':
-            return 1;
-        case '3':
-        case '5':
-        case '9':
-        case '6':
-        case 'a':
-        case 'c':
-            return 2;
-        case '7':
-        case 'b':
-        case 'd':
-        case 'e':
-            return 3;
-        case 'f':
-            return 4;
+function countUsedCells(grid: number[][]): any {
+    return grid.reduce((total, row) => {
+        return total + row.reduce((rowTotal, cell) => {
+            return rowTotal + (cell >= 0 ? 1 : 0);
+        }, 0);
+    }, 0);
+}
+
+function setBitsInRow(hashString: string) {
+    let row: number[] = [];
+    hashString.split('').forEach((hexDigit, i) => {
+        let byte = parseInt(hexDigit, 16);
+        for (let offset = 1; offset <= 4; offset++) {
+            row[4 * (i + 1) - offset] = byte % 2 === 1 ? 0 : -1
+            byte = Math.floor(byte / 2);
+        }
+    });
+
+    return row;
+}
+
+function countGroupsInGrid(grid: number[][]): number {
+    let groupCount = 1;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === 0) {
+                markGroup(grid, i, j, groupCount++);
+            }
+        }
     }
 
-    throw new Error('Invalid hex digit');
+    return groupCount - 1;
+}
+
+interface Coord {
+    x: number;
+    y: number;
+}
+
+function markGroup(grid: number[][], row: number, col: number, groupId: number) {
+    let queue: Coord[] = [{ x: row, y: col }];
+
+    grid[row][col] = groupId;
+
+    while (queue.length) {
+        let { x, y } = queue.shift()!;
+
+        [
+            { x: x - 1, y },
+            { x: x + 1, y },
+            { x, y: y - 1 },
+            { x, y: y + 1 },
+        ].forEach(coord => {
+            let { x, y } = coord;
+            if (grid[x] && grid[x][y] === 0) {
+                grid[x][y] = groupId;
+                queue.push({
+                    x,
+                    y
+                });
+            }
+        });
+    }
 }
 
 findSolution();
