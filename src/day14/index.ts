046ad87fc6af2be76d9a9838dcb0c9ec5d5b236e
Max Vaysburd <maxvay@microsoft.com>
2017-12-21 17:52:34 -0800

    Day 14, part 1.

--- src/day14/index.ts
+++ src/day14/index.ts
@@ -1,7 +1,37 @@
 import * as fs from 'mz/fs';
+import {knotHash} from '../day10/knotHash'; 
 
 async function findSolution(inputFilePath: string = './input.txt') {
     const input = await fs.readFile(require.resolve(inputFilePath), 'utf8');
+    var hashes : string[] = new Array(128);
+    var setBitCount = 0;
+    for (var i=0; i<128; i++)
+    {
+        hashes[i] = knotHash(input+'-'+i);
+        setBitCount += countBitsInString(hashes[i]);
+    }
+
+    console.log(setBitCount);
 }
 
-findSolution();
+function countBits(n: number) : number {
+    var count=0;
+    while (n)
+    {
+        n &= (n-1);
+        count++;
+    }
+    return count;
+}
+
+function countBitsInString(input : string) : number {
+    var count =0;
+    for (var i=0; i< input.length; i++)
+    {
+        count += countBits(Number.parseInt(input[i], 16));
+    }
+    return count;
+}
+
+ findSolution();
+// console.log(countBits(9));
